0b7aba7edc2149c0f396e0de202080d58371720e
mmitchell25-dos <mmitchell25@elam.co.uk>
2025-11-19 15:38:10 +0000

    yep

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,141 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody niko;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck;
+
+
+
+    void Start()
+    {
+        niko = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        niko.linearVelocity = new Vector3(horizontalInput * 5f, niko.linearVelocity.y, verticalInput * 5f);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            niko.linearVelocity = new Vector3(niko.linearVelocity.x, jumpForce, niko.linearVelocity.z);
+        }
+
+
+
+
+
+
+    }
+
+    bool IsGrounded()
+    {
+        Physics.CheckSphere(groundCheck.position, .1f, )
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
